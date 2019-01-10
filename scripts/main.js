2753fa6a7d9e815718739ca7101355144e12bcc8
vasishchev <dmitry.vasishev@gmail.com>
2019-01-10 17:42:25 -0500

    add js stuff

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+  // Define a Function
+ function sayOuch() {
+ 	alert('Ouch! Stop poking me!');
+ }
+ 
+ // Use a CSS selector to identify an element
+ var foxImage = document.querySelector('img');
+ 
+ // Assign the function to the onclick event on that element
+ foxImage.onclick = sayOuch;
